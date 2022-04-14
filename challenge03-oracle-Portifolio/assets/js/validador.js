cdf604adff6af41f020b7977774cfc96ef3bb8ba
Edsondomingos <edomingoswork@gmail.com>
2022-04-14 01:35:26 -0300

    codigo javascript no portfolio

--- challenge03-oracle-Portifolio/assets/js/validador.js
+++ challenge03-oracle-Portifolio/assets/js/validador.js
@@ -1,4 +1,35 @@
-// let form = document.querySelector('form');
-// form.addEventListener('submit', e => {
-//     e.preventDefault(); 
-// })
+let form = document.querySelector('form');
+let bt = document.querySelector('[data-submit]');
+let nome = document.getElementById('nome');
+let email = document.getElementById('email');
+let assunto = document.getElementById('assunto');
+let msg = document.getElementById('msg');
+
+function verifica() {
+    if (nome.value == ''){
+        nome.setCustomValidity('Coloque seu nome para contato');
+    } else {
+        nome.setCustomValidity('');
+    }
+
+    if (email.value == ''){
+        email.setCustomValidity('Deixe seu email para retorno');
+    } else {
+        email.setCustomValidity('');
+    }
+
+    if (assunto.value == ''){
+        assunto.setCustomValidity('Qual o seu interesse?');
+    } else {
+        assunto.setCustomValidity('');
+    }
+
+    if (msg.value == ''){
+        msg.setCustomValidity('Me de alguns detalhes');
+    } else {
+        msg.setCustomValidity('');
+    }
+
+}
+
+bt.addEventListener('click', verifica)
