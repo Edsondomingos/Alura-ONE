5edd396d2473fe2c057606188f54100cfc709a4b
Edson <edomingoswork@gmail.com>
2022-06-22 11:35:51 -0300

    update de telas conversor

--- challenge05-oracle-ConversorDeMoeda/conversor/src/conversor/Conversor.java
+++ challenge05-oracle-ConversorDeMoeda/conversor/src/conversor/Conversor.java
@@ -9,27 +9,61 @@ import javax.swing.JOptionPane;
 public class Conversor {
 
     public static void main(String[] args) {
+    	
+    	Object jop = JOptionPane();
 //        double numero = Double.parseDouble(JOptionPane.showInputDialog("Escreva um " +;
 //                "numero para multiplicar por 2"));
 //        Object cor = JOptionPane.showInputDialog(null, "Escolha uma cor", "Cores",
 //                JOptionPane.QUESTION_MESSAGE, null,
 //                new Object[] {"Escolha","Amarelo","Azul","Vermelho" },"Escolha");
 
-//        Object tipoConversor = JOptionPane.showInputDialog(null, "Escolha uma opÃ§Ã£o", 
+//        Object tipoConversor = JOptionPane.showInputDialog(null, "Escolha uma opção", 
 //                "Tipo", JOptionPane.QUESTION_MESSAGE, null,
 //             new Object[] {"Conversor de Moeda","Conversor de Temperatura" },"Escolha");
 
 //        Object valor = JOptionPane.showInputDialog("");
 
-         Object moedas = JOptionPane.showInputDialog(null, "Escolha a moeda a qual vocÃª deseja converter", 
-                "Tipo", JOptionPane.WARNING_MESSAGE, null,
-             new Object[] {"De Reais para Dolares","De Reais para Euros","De Reais para Libras",
-             "De Reais para Yenes","De Reais para Won Coreano", "De Dolares para Reais","De Euros para Reais",
-             "De Libras para Reais"},"Escolha");
+//         Object moedas = JOptionPane.showInputDialog(null, "Escolha a moeda a qual você deseja converter", 
+//                "Tipo", JOptionPane.WARNING_MESSAGE, null,
+//             new Object[] {"De Reais para Dolares","De Reais para Euros","De Reais para Libras",
+//             "De Reais para Yenes","De Reais para Won Coreano", "De Dolares para Reais","De Euros para Reais",
+//             "De Libras para Reais"},"Escolha");
+         
+//         Object resultado = JOptionPane.showMessageDialog(null, "O valor da conversão é de ", "Resultado", 
+//        		 JOptionPane.INFORMATION_MESSAGE);
+//         JOptionPane.showConfirmDialog(null, "ConfirmDialog");
+    	
+    	// DEU CERTO - 
+//    	JOptionPane.showMessageDialog(null, JOptionPane.MESSAGE_PROPERTY);
+//      JOptionPane.showMessageDialog(null,"O valor da conversão", "Resultado",1);
+    	
+    	// Frame quando erro de tipo
+//    	JOptionPane.showMessageDialog(null,"Valor inválido", "Erro",1); 
+    	
+    	// Frame final da conversão
+//    	JOptionPane.showConfirmDialog(null, "Deseja continuar?", "E AGORA?", 1);
+
+    	// Frame caso click em NO na tela final
+    	JOptionPane.showMessageDialog(null, "Programa finalizado","FIM",1);
+    	
+//    	JOptionPane.showInputDialog("Qual o seu nome?", "Edson"); // Janela titulo e nome ja escrito no input
+    	
+    	
+    	
+    	// 4 argumentos no showInput Dialog()
+    	// 1 - De qual frame é dependente, se não há, coloca null
+    	// 2 - Mensagem Exibida
+    	// 3 - Titulo da caixa
+    	// 4 - Tipo qual tipo de icone na mensagem, definido pelas constantes ou pelos inteiros que as representão
+		//    	PLAIN_MESSAGE (valor: -1): Mensagem limpa, sem nenhum ícone.
+		//    	ERROR_MESSAGE (valor: 0): Mensagem de erro.
+		//    	INFORMATION_MESSAGE (valor: 1): Mensagem informativa.
+		//    	WARNING_MESSAGE (valor: 2): Mensagem de alerta.
+		//    	QUESTION_MESSAGE (valor: 3): Mensagem de requisição ou pergunta.
+         
         
         
     }
     
 }
 
-//WARNING_MESSAGE
